89c4f1d22ce35f1e037581e58eba38c77fe08aac
Mikael Dubik <mkd@dohop.com>
2016-09-12 23:12:50 +0200

    Power method finished :)

--- python_1/power_method.py
+++ python_1/power_method.py
@@ -1,47 +1,81 @@
-#Implement power method here, accept command line input
-# and spit out value
-from __future__ import division
 from pprint import pprint
-from pdb import set_trace
 from copy import deepcopy
+import random
+import math
+import time
+import numpy as np
 
 class power_method(object):
 
 	@staticmethod
 	def calculate(N, epsilon, method):
-		# result size is mxn * nxp = mxp
+		assert(isinstance(N, (int, long)))
+		assert(isinstance(epsilon, float))
+		assert(isinstance(method, basestring))
 
-		matrix = [[2, -12], [1, -5]]
+		if method == 'NumPy':
+			start = time.time()
+			return power_method._numpy(N, epsilon), time.time() - start
 
-		result, eigenvector = [], []
+		start = time.time()
+		return power_method._normal(N, epsilon), time.time() - start
 
-		for i in xrange(2):
-			result.append([1])
-			eigenvector.append([1])
+	@staticmethod
+	def _normal(N, epsilon):
+		#A, x0, N = [[2.0, -12.0], [1.0, -5.0]], [[1.0], [1.0]], 2
+		#A, x0, N = [[1.0, 1.0], [0.0, -1.0]], [[1.0], [1.0]], 2
+		#A, x0, N = [[6.0, 3.0, 2.0], [7.0, 2.0, 3.0], [5.0, 5.0, 1.0]], [[1.0], [1.0], [1.0]], 3
 
-		approximation = deepcopy(eigenvector)
+		A, x0 = [], []
+		for i in xrange(N):
+			A.append([float(random.randint(-100, 100)) for x in xrange(N)])
+			x0.append([1.0])
 
-		for i in xrange(6):
+		xk, yk = deepcopy(x0), deepcopy(x0)
+
+		for i in xrange(100000):
 			# Dot product
-			for j in xrange(len(matrix)):
-				tmp = 0
-				for k in xrange(len(eigenvector)):
-					tmp += matrix[j][k] * eigenvector[k][0]
+			for j in xrange(N):
+				yk[j][0] = 0.0
+				for k in xrange(N):
+					yk[j][0] += A[j][k] * x0[k][0]
 
-				result[j] = [tmp]
+			norm_max_yk = max(yk)[0]
 
+			for j in xrange(N):
+				xk[j][0] = yk[j][0] / norm_max_yk
 
+			error = math.sqrt(math.pow(sum([xk[j][0] - x0[j][0] for j in xrange(N)]) , 2) / N)
 
+			if error < epsilon:
+				return xk, i
 
-			eigenvector = deepcopy(result)
+			x0 = deepcopy(xk)
 
-			for j, x in enumerate(eigenvector):
-				approximation[j][0] = eigenvector[j][0] / eigenvector[-1][0]
+	@staticmethod
+	def _numpy(N, epsilon):
+		#A, x0 = np.array([[2, -12], [1, -5]]), np.ones(2).transpose()
+		#A, x0 = np.array([[1, 1], [0, -1]]), np.ones(2).transpose()
+		#A, x0 = np.array([[6, 3, 2], [7, 2, 3], [5, 5, 1]]), np.ones(3).transpose()
+		A, x0 = np.random.randint(-100, 100, (N,N)), np.ones(N).transpose()
+		for i in xrange(100000):
+			yk = A.dot(x0)
+			xk = yk / max(yk)
+			error = np.sqrt(np.power((np.sum(xk - x0)), 2)/2)
 
-			print eigenvector
+			if error < epsilon:
+				return xk, i
 
+			x0 = xk
 
 if __name__ == '__main__':
-	power_method.calculate(3, 0.00006, 'Normal')
+	# This block only used for testing
+	import argparse
+	parser = argparse.ArgumentParser()
+	parser.add_argument('n', const = 2, type = int, nargs = '?')
+	parser.add_argument('eps', const = 0.0006, type = float, nargs = '?')
+	parser.add_argument('numpy', const = False, type = str, nargs = '?')
+	args = parser.parse_args()
 
-
+	#print power_method.calculate(args.n, args.eps, 'NumPy')
+	print power_method.calculate(args.n, args.eps, args.numpy)
